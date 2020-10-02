4cbc51a3ed690b7f3f39f563fb3085a80d7530e7
root <root@localhost.localdomain>
2020-10-02 17:18:20 -0400

    fixed naming mistake in AntInstall.sh rpm package

--- bashS/AntInstall.sh
+++ bashS/AntInstall.sh
@@ -5,7 +5,7 @@ echo "Installing Ant"
 
 wget http://mirror.centos.org/centos/7/os/x86_64/Packages/ant-junit-1.9.4-2.el7.noarch.rpm
 
-sudo yum install ant-junit-1.9.4-2.e17.noarch.rpm
+sudo yum install ant-junit-1.9.4-2.el7.noarch.rpm
 
 echo "Done"
 
