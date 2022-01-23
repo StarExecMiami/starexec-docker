69d5fcce2fa64491ce5b670daeb2a9d0d4cf89bd
Abdullah Zahid <mxz460@miami.edu>
2022-01-22 21:14:19 -0500

    deletes swapfiles

--- dockerPackage/allScripts/dockerScripts/DockerAppBuild.sh
+++ dockerPackage/allScripts/dockerScripts/DockerAppBuild.sh
@@ -8,16 +8,9 @@
 #starts tomcat server
 su -c "/project/apache-tomcat-7/bin/catalina.sh run &" tomcat
 
-sleep 10 #to give some time for servers to start up
-
-#if the db wasn't created before it will be now
+sleep 10 
 
 sudo mysql -u root -e "DROP DATABASE IF EXISTS starexec; CREATE DATABASE starexec; GRANT ALL PRIVILEGES ON starexec.* TO 'se_admin'@'localhost' IDENTIFIED BY 'dfsdf34RFerfg3TFGRfrF3edFVg12few2'; FLUSH PRIVILEGES;"
 
-#to get volumes working these next lines encapsulate vital info for persistant data
-
-mkdir volumeData && cd volumeData
-ln -s ../home/starexec && ln -s ../project/ && ln -s ../local/sandbox
-cd ..
 
 bash allScripts/starexecScripts/AntBuildDeploy.sh
--- dockerPackage/allScripts/starexecScripts/AntBuildDeploy.sh
+++ dockerPackage/allScripts/starexecScripts/AntBuildDeploy.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-
-#firefox http://localhost:8080/manager
-
-#how do we add the username and password?
-
-#sudo mysql -u root -e "DROP DATABASE starexec; CREATE DATABASE starexec; GRANT ALL PRIVILEGES ON starexec.* TO 'se_admin'@'localhost' IDENTIFIED BY 'dfsdf34RFerfg3TFGRfrF3edFVg12few2'; FLUSH PRIVILEGES;"
+sudo mysql -u root -e "DROP DATABASE starexec; CREATE DATABASE starexec; GRANT ALL PRIVILEGES ON starexec.* TO 'se_admin'@'localhost' IDENTIFIED BY 'dfsdf34RFerfg3TFGRfrF3edFVg12few2'; FLUSH PRIVILEGES;"
 
 
 cd ~starexec/StarExec-deploy/sql && mysql -u root starexec < NewInstall.sql
